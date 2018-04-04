d215026ee78fb713c2fad6124289bded04c72b89
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2018-04-04 19:10:34 +0530

    page 1 created
    mini effective enterence

--- class.cpp
+++ class.cpp
@@ -1,1 +1,16 @@
-//Include this file in "class.h"
+class enterence 
+{
+  public:
+  void page1()
+  {
+   char b=2,a[]="welcome to beta note maker";
+   clrscr();
+   gotoxy(25, 12);
+   for(int i=0;i<strlen(a);++i)
+   {
+    delay(100);
+    cout<<a[i];
+   }
+   cout<<b;
+  }
+};
