0178a9d7ea419135dc2cb1fc268010a82d5f4c05
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2018-04-14 18:12:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,29 +9,35 @@ void main()
   switch(option)
     {
      case '1' :  {                                                         // Check today's dashboard
-                   ifstream fin;
-                   fin.open("",ios::in);
-                   fin.read((char*)rem,sizeof(*rem));
-
-                   rem->display();
+                          ifstream fin;
+                          fin.open("",ios::in);
+                          fin.read((char*)rem,sizeof(*rem));
+       
+                          rem->display();
+                          getch();
+                          goto opt;
                  }
      case '2' :  {                                                          //Set reminder
-                    ofstream fout;
-                    fout.open("",ios::app|ios::ate);
-                    if(!fout)
-                    {
-                    cout<<"File not exist ";
-                    exit(1);}
-                    rem->read();
-                    fout.write((char*)rem,sizeof(*rem));
-       
+                          ofstream fout;
+                          fout.open("",ios::app|ios::ate);
+                          if(!fout)
+                          {
+                          cout<<"File not exist ";
+                          exit(1);}
+                          rem->read();
+                          fout.write((char*)rem,sizeof(*rem));
+
+                          getch();
+                          goto opt;
                  }
      case '3' :  {                                                          //Show events
-                    ifstream fin;
-                    fin.open("",ios::in);
-                    fin.read((char*)rem,sizeof(*rem));
+                          ifstream fin;
+                          fin.open("",ios::in);
+                          fin.read((char*)rem,sizeof(*rem));
 
-                    rem->display();
+                          rem->display();
+                          getch();
+                          goto opt;
                   }
      default  :  {
                        cout<<"Invalid option";
