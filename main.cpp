917c0f67b1e80497ffbeaac852b5a5c35db89ea1
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2018-04-14 18:05:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,42 @@
 #include<header.h>
 void main()
   {
-   display view;
+  display view;
+  remainder *rem;
   view.page1();
   opt: clrscr();
   char option=view.options();
   switch(option)
     {
-     case '1' : 
-     case '2' :
-     case '3' :
-     default  : {
+     case '1' :  {                                                         // Check today's dashboard
+                   ifstream fin;
+                   fin.open("",ios::in);
+                   fin.read((char*)rem,sizeof(*rem));
+
+                   rem->display();
+                 }
+     case '2' :  {                                                          //Set reminder
+                    ofstream fout;
+                    fout.open("student.txt",ios::app|ios::ate);
+                    if(!fout)
+                    {
+                    cout<<"File not exist ";
+                    exit(1);}
+                    rem->read();
+                    fout.write((char*)rem,sizeof(*rem));
+       
+                 }
+     case '3' :  {                                                          //Show events
+                    ifstream fin;
+                    fin.open("",ios::in);
+                    fin.read((char*)rem,sizeof(*rem));
+
+                    rem->display();
+                  }
+     default  :  {
                        cout<<"Invalid option";
                        goto opt;
-                }
+                 }
      }
   }
        
